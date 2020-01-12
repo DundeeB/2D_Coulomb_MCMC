7f4133265bf078b0d66ced49f772750ec953bfa2
DundeeB <ligolas@gmail.com>
2020-01-12 19:33:41 +0200

    Moving to more physical units

--- metropolis_step.m
+++ metropolis_step.m
@@ -1,10 +1,14 @@
-function [state, q] = metropolis_step(state, i, dr)
+function [state, accept_flag] = metropolis_step(state, i, dr)
 orig = state.spheres;
-state.spheres(i,:) = cyclic(state.spheres(i,:) + dr, state.cyclic_boundary);
-q = 1;
-if ~legal_configuration(state, i)
-    state.spheres = orig;
-    q = 0;
+E0 = Energy(state, i);
+new_state = state;
+new_state.spheres(i,:) = cyclic(state.spheres(i,:) + dr, state.cyclic_boundary);
+E1 = Energy(new_state, i);
+accept_flag = 0;
+p = min(1, exp(-state.beta*(E1-E0)));
+if rand < p  % accept step if E1-E0<0, or randomally accept it if E1-E0>0
+    state = new_state;
+    accept_flag = 1;
 end    
 end
 
--- run2D_coulomb.m
+++ run2D_coulomb.m
@@ -1,89 +1,52 @@
-addpath(
-rho_H_arr = [0.95 1 0.6 0.65 0.7 0.8:0.05:1];
-I = ones(1, length(rho_H_arr)-2);
-h_arr = [1 1 0.9*I];
-I = ones(1, length(h_arr));
-n_row_arr = 2*50*I;
-n_col_arr = 2*18*I;
+clear all; units;
+cd('C:\Users\Daniel Abutbul\OneDrive - Technion\2D Coulomb Naive');
+addpath ../'3D Metropolis Monte Carlo'/;
+T = [3e3*Kelvin];  % beta = beta*K*q^2, where exp(-beta*H)=exp(-beta*Energy) and energy is 1/r
+I = ones(1, length(beta_tilde_arr));
+eta_arr = 0.25*I;  % eta = N*sig^2/A
+n_row_arr = 10*I;
+n_col_arr = 10*I;
 
-IC_pool = {'square','triangle','AF_triangle'};
-Initial_Conditions_arr(1:length(n_col_arr)) = {IC_pool{3}};
+N_real = 1e2;
+f = 0.5;  % factor step size
 
 code_dir = pwd;
-father_dir = 'C:\Users\Daniel\OneDrive - Technion\simulation-results\';
+simulations_dir = 'C:\Users\Daniel Abutbul\OneDrive - Technion\simulation-results-Coulomb\';
 
 for j = 1:length(n_col_arr)
     tic;
-    rho_H = rho_H_arr(j);
-    h = h_arr(j);
     n_row = n_row_arr(j);
     n_col = n_col_arr(j);
-    N = n_row * n_col;
-    C = Initial_Conditions_arr(j);
-    Initial_Conditions = C{1};
     
-    state.rad = 1;
-    sig = 2*state.rad;
-    state.H = (h+1)*sig;
-
-    switch Initial_Conditions
-        case IC_pool{1}
-            state.cyclic_boundary = sqrt(1/(rho_H*(1+h)))*sig*sqrt(N)*[1 1];
-            state.spheres = antiferro_rect_starting_cond3D([n_col n_row 1],...
-                [state.cyclic_boundary state.H],state.rad);
-            sim_name = ['N=' num2str(N) '_h=' num2str(h), '_rhoH=' ...
-                num2str(rho_H) '_' IC_pool{1}]
-    	case IC_pool{2}
-            a = sqrt(sig^2/(rho_H*(h+1)*sin(pi/3)));
-            Lx = n_col*a;
-            Ly = n_row*a*sin(pi/3);
-            state.cyclic_boundary = [Lx Ly];
-            state.spheres = ferro_triang_starting_cond3D([n_col n_row 1],...
-                [state.cyclic_boundary state.H],state.rad);
-            sim_name = ['N=' num2str(N) '_h=' num2str(h), '_rhoH=' ...
-                num2str(rho_H) '_' IC_pool{2}]
-        case IC_pool{3}
-            a = sqrt(2*sig^2/(rho_H*(h+1)*sin(pi/3)));
-            Lx = n_col*a;
-            Ly = n_row/2*a*sin(pi/3);
-            state.cyclic_boundary = [Lx Ly];
-            state.spheres = antiferro_triang_starting_cond3D...
-                ([n_col n_row 1],[state.cyclic_boundary state.H],state.rad);
-            sim_name = ['N=' num2str(N) '_h=' num2str(h), '_rhoH=' ...
-                num2str(rho_H) '_' IC_pool{3}]
-    end
-    A = state.cyclic_boundary(2)*state.cyclic_boundary(1);
-    eta2D = N*pi*state.rad^2/A;
-    eta3D = N*4*pi/3*state.rad^3/(A*state.H);
-    title_name = ['N=' num2str(N) ', h=' num2str(h), ', \rho_H=' num2str(rho_H)];
+    N = n_row * n_col;
+    beta = 1/(k_B*T(j));
+    state.rad = r_ion;  % hard sphere rejection still exist
+    A = N*(2*state.rad)^2/eta_arr(j);
+    
+    state.H = 4;  % z>H/2 -> charge = +1, z< H/2-> chage = -1
+    state.beta = beta;
+    state.cyclic_boundary = sqrt(A)*[1 1];
+    state.spheres = antiferro_rect_starting_cond3D([n_col n_row 1],...
+        [state.cyclic_boundary state.H],state.rad);
+    sim_name = ['N=' num2str(N) '_eta=' num2str(eta_arr(j)) '_T=' num2str(T(j))]
+    title_name = ['N=' num2str(N) ' \eta=' num2str(eta_arr(j)) ' T=' num2str(T(j)) 'K'];
     %%
     addpath('.');
-    cd(father_dir); mkdir(sim_name); cd(sim_name);
+    cd(simulations_dir); mkdir(sim_name); cd(sim_name);
     %%
-    if ~legal_configuration(state,1)
-        cd(code_dir);
-        error('too many spheres');
-    end
-
-    N_real  = 1e4*N/9*5;
-    L = state.H;
-    f = 0.2;
-    step_size = f*(sqrt((state.cyclic_boundary(2)/n_row)^2+(state.cyclic_boundary(1)/n_col)^2)-2*state.rad);
-    N_save = 1e4;
-    N_start = 1e4;
+%     N_real  = 100;  % 1e4*N/9*5;  % TBD!
+    a = sqrt(A/N);
+    step_size = f*a;
+    N_save = 1;  % 1e4;  % TBD!
+    N_start = 1;  % 1e4;  % TBD!
     %%
     save('Input_parameters');
     %%
     q = 0;
     for i=1:N_real
         t = rand*2*pi;
-        if step_size > state.H
-            phi = (2*rand-1)*(asin(state.H/step_size));
-        else
-            phi = rand*pi;
-        end
         i_p = randi(N);
-        [state, q_] = metropolis_step(state, i_p, step_size*[sin(phi)*cos(t) sin(phi)*sin(t) cos(phi)]);
+        [state, q_] = metropolis_step(state, i_p, step_size*[cos(t) sin(t) 0]);
         q = q + q_;
         if mod(i,N_real/10)==0
             disp([num2str(i/N_real*100) '%']);
@@ -95,20 +58,20 @@ for j = 1:length(n_col_arr)
     end
     %% write first and last sphere
     subplot(1,2,2); 
-    plot_spheres(state); 
+    plot_circles(state); 
     title([num2str(i) ' step. Acceptance rate: ' ...
         num2str(100*q/i) '%, steps per sphere: ' num2str(q/N)]);
     load('Input_parameters');
     subplot(1,2,1); 
-    plot_spheres(state); 
+    plot_circles(state); 
     title(title_name);
     savefig('Starting_and_Final_configuration.fig')
-    close all;
+%     close all;
     %%
     cd(code_dir);
     toc;
     %%
-    tic;
-    post_process([father_dir sim_name],false, 'output_psi14_psi23_b1_N_sp_100',100);
-    toc;
+%     tic;
+%     post_process([simulations_dir sim_name],false, 'output_psi14_psi23_b1_N_sp');  % _100',100);  % TBD
+%     toc;
 end
